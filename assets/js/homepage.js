0d41e2b17527dbbc74c1d02dbf510604431c571d
DM <bmdmartinez@gmail.com>
2021-06-27 13:59:59 -0400

    added functionality to search for projects by code language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event) {
     // prevent page from refreshing
@@ -18,6 +19,17 @@ var formSubmitHandler = function(event) {
         nameInputEl.value = "";
     } else {
         alert("Please enter a GitHub username");
+    }
+};
+
+var buttonClickHandler = function(event){
+    var language = event.target.getAttribute("data-language");
+   
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
     }
 };
 
@@ -41,6 +53,20 @@ var getUserRepos = function(user) {
     .catch(function(error){
         // Notice this '.catch()' getting chaines onto the end of the '.then()' method
         alert("Unable to connect to GitHub");
+    });
+};
+
+var getFeaturedRepos = function() {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response){
+        if (response.ok) {
+            response.json().then(function(data){
+                displayRepos(data.items, language);
+            });  
+        } else {
+            alert('Error: ' + response.statusText);
+        }
     });
 };
 
@@ -91,7 +117,8 @@ var displayRepos = function(repos, searchTerm) {
         // append container to the dom
         repoContainerEl.appendChild(repoEl);
     }
-}
+};
 
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
